484b743d0ad52164df2dd439db354e05c001597f
Rosa Carranza <rcarranza1024@yahoo.com>
2020-06-10 21:55:31 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -5,9 +5,11 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
+  'SELECT gpa FROM students ORDER BY gpa ASC LIMIT 1'
 end
 
 def average_student_gpa
+  
 end
 
 def total_tardies_for_all_students
